ff129a089626b84011cb90fb96fba254ce0dd2b1
chandekarmilind <87879442+chandekarmilind@users.noreply.github.com>
2022-07-15 01:43:51 +0530

    Delete FirstStudent copy directory

--- FirstStudent copy/modules/vpc/outputs.tf
+++
@@ -1,6 +0,0 @@
-output "vpc_id" {
-  value       = aws_vpc.main.id
-  description = "VPC id."
-  # Setting an output value as sensitive prevents Terraform from showing its value in plan and apply.
-  sensitive = false
-}
